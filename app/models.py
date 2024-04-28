0f981de53b7a65e8aedf0c8f34dcae10e60ba6a3
Chensu Yang <24035732@student.uwa.edu.au>
2024-04-29 00:31:19 +0800

    Sign Up Func
    Now you can sign up your personal details(maybe not personal) into the database file now.(It will automatically create one if you do not use the app.db file)
    
    New page as sign up
    Added blueprint
    Delete the page which used to be index

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 import os
-import uuid
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -21,19 +20,12 @@ Migrate(app, db)
 
 class User(db.Model):
 
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def __init__(self):
-        self.id = uuid.uuid4()
-        self.username = self.email
-        self.email = None
+    password = db.Column(db.String(80), nullable=False)
 
 
 class UserDetails(db.Model):
@@ -43,11 +35,6 @@ class UserDetails(db.Model):
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     address = db.Column(db.Text)
     account = db.Column(db.Float)
-    products = db.relationship('Product', backref='seller', lazy=True)
-    transactions = db.relationship('Transaction', backref='buyer', lazy=True)
-
-    def __repr__(self):
-        return '<User %r>' % self.id
 
 
 class Product(db.Model):
@@ -59,34 +46,16 @@ class Product(db.Model):
     description = db.Column(db.String(200))
     img = db.Column(db.String(200))
     price = db.Column(db.Float, nullable=False)
-    seller_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-
-    def __repr__(self):
-        return '<Product %r>' % self.title
-
-    def __init__(self, title, description, img, price, seller_id, category_id):
-        self.title = title
-        self.description = description
-        self.img = img
-        self.price = price
-        self.seller_id = seller_id
-        self.category_id = category_id
-        self.seller = UserDetails()
+    seller_id = db.Column(db.Integer, db.ForeignKey('user_details.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
 
 
 class Category(db.Model):
 
-    __tablename__ = 'category'
+    __tablename__ = 'categories'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<Category %r>' % self.name
-
-    def __init__(self, name):
-        self.name = name
 
 
 class Transaction(db.Model):
@@ -94,20 +63,13 @@ class Transaction(db.Model):
     __tablename__ = 'transactions'
 
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
     buyer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     total = db.Column(db.Float, nullable=False)
     transaction_time = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<Transaction %r>' % self.id
 
-    def __init__(self, product_id, buyer_id, quantity, total, transaction_time):
-        self.product_id = product_id
-        self.buyer_id = buyer_id
-        self.quantity = quantity
-        self.total = total
-        self.transaction_time = transaction_time
+
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,36 +1,48 @@
-from app import app
-from app.forms import LoginForm
-from flask import render_template, flash, redirect, url_for
-from flask_login import LoginManager, login_user, current_user
-from app.models import User
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_login import login_user, current_user
+from .forms import LoginForm, SignUpForm
+from .models import db, User
 
-@app.route('/')
-@app.route('/index')
-def index():
+main = Blueprint('main', __name__)
 
-    return render_template('index.html', title="Main")
-
-
-@app.route('/login', methods=['GET', 'POST'])
+@main.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('main.login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        return redirect(url_for('main.home'))
     return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/home')
-def home():
+@main.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            flash('The email is already registered')
+            return redirect(url_for('main.signup'))
+        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Successfully registered!')
+        return redirect(url_for('main.login'))
+    return render_template('signup.html', title='Sign Up', form=form)
 
+@main.route('/')
+@main.route('/home')
+def home():
     return render_template('home.html', title='Home')
 
-@app.route('/market')
-def market():
+@main.route('/profile')
+def profile():
+    return render_template('profile.html', title='Profile')
 
+@main.route('/market')
+def market():
     return render_template('market.html', title='Home')
