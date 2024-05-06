3a709112631ecd28af1de715a6a0acb9f0b05936
MingyuLian <142514410+MingyuLian@users.noreply.github.com>
2024-05-06 23:18:00 +0800

    Base update and Home first version

---
+++ app/static/myScript.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const authRequiredLinks = document.querySelectorAll('.requires-auth');
+    authRequiredLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            if (!userIsAuthenticated()) {
+                event.preventDefault(); // 阻止链接的默认行为
+                var myModal = new bootstrap.Modal(document.getElementById('loginModal'));
+                myModal.show(); // 显示模态框
+            }
+        });
+    });
+});
+
+function userIsAuthenticated() {
+    return document.body.dataset.userAuthenticated === 'true';
+}
+
