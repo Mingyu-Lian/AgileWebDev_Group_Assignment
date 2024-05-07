0326b17140f8c590e1fc7affa07fcffed8c42993
MingyuLian <142514410+MingyuLian@users.noreply.github.com>
2024-05-07 09:43:38 +0800

    Home page Update

--- app/static/myScript.js
+++ app/static/myScript.js
@@ -15,3 +15,9 @@ function userIsAuthenticated() {
     return document.body.dataset.userAuthenticated === 'true';
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
