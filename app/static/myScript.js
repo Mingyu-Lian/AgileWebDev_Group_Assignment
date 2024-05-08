944883e6e2f348ac049c561a9e470c8cbff0a1a5
MingyuLian <142514410+MingyuLian@users.noreply.github.com>
2024-05-08 23:03:30 +0800

    Front-end for signup/login/profile etc pages

---
+++ app/static/myScript.js
@@ -0,0 +1,12 @@
+function userIsAuthenticated() {
+    return document.body.dataset.userAuthenticated === 'true';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
+
+
