5094c7cd30b056f2883b27afb31ca4e18591eecb
Chensu Yang <24035732@student.uwa.edu.au>
2024-05-15 14:57:10 +0800

    bug fix
    changed the default port

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Config:
     UPLOAD_POST_IMG = os.path.join(basedir, 'static', 'post_image')
     DEFAULT_POST_IMAGE_PATH = os.path.join(basedir, 'static', 'post_image', 'default.jpg')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
-    SERVER_NAME = os.environ.get('SERVER_NAME') or 'localhost:5000'
+    SERVER_NAME = os.environ.get('SERVER_NAME') or 'localhost:8080'
     APPLICATION_ROOT = os.environ.get('APPLICATION_ROOT') or '/'
     PREFERRED_URL_SCHEME = os.environ.get('PREFERRED_URL_SCHEME') or 'http'
 
